10994252edbc2c6f4fecb1ac8585713eabf8c9cc
Evgenya888 <e.v.a240967@gmail.com>
2024-09-26 11:38:31 +0700

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -2,7 +2,7 @@ n = int(input('Введите целое число от 3 до 20: '))
 
 
 def get_password(number):
-    password = ''
+    password = []
     for i in range(1, number):
         for j in range(2, number):
             if j <= i:
