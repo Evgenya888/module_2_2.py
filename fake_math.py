241754d05b7167df75fda4374cb81d78c915ff81
Evgenya888 <e.v.a240967@gmail.com>
2024-10-21 18:04:33 +0700

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,30 @@
+def divide(first, second):
+
+    if second  != 0:
+        print(first / second)
+    else:
+        print('Ошибка')
+        return
+
+
+
+
+
+divide(69, 3)
+divide(3, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
