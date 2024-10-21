373ae15c4794e1ebcaf014b6ae2ab73019fc6297
Evgenya888 <e.v.a240967@gmail.com>
2024-10-21 18:06:06 +0700

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,15 @@
+from math import inf
+def divide(first , second):
+
+    if second  != 0:
+        print(first / second)
+    else:
+        print(inf)
+        return inf
+
+
+
+
+
+divide(49, 7)
+divide(15, 0)
